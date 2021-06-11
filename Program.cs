7b6818415e7f32f4e1de988ea2b70e603fc2b9a3
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:48:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS011StringSplitJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Enter a digit: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //// Part A: "num num num num" using Write
+            //Console.Write(num);
+            //Console.Write(" ");
+            //Console.Write(num);
+            //Console.Write(" ");
+            //Console.Write(num);
+            //Console.Write(" ");
+            //Console.Write(num);
+            //Console.WriteLine();
+            Console.WriteLine("{0}", OpakovatText(num.ToString()," ", 8));
+
+            //// Part B: "numnumnumnum" using Write
+            //Console.Write(num);
+            //Console.Write(num);
+            //Console.Write(num);
+            //Console.WriteLine(num);
+            //Console.WriteLine();
+            Console.WriteLine("{0}", OpakovatText(num.ToString(), "", 8));
+
+            //// Part C: "num num num num" using {0}
+            //Console.WriteLine("{0} {0} {0} {0}", num);
+            Console.WriteLine("{0}", OpakovatText(num.ToString(), " ", 8));
+
+            //// Part D: "numnumnumnum" using {0}
+            //Console.WriteLine("{0}{0}{0}{0}", num);
+            Console.WriteLine("{0}", OpakovatText(num.ToString(), "", 8));
+
+            Console.ReadKey();
+        }
+
+        static string OpakovatText(string text,string oddelovac, int pocet)
+        {
+            if (pocet == 0) return ("");
+            return 
+                string.Join(
+                    oddelovac, 
+                    (string.Format(
+                        "{0};{1}",
+                        text, 
+                        OpakovatText(
+                            text,
+                            oddelovac,
+                            pocet - 1))).Split(';'));
+        }
+    }
+}
